e9af6ee2334fc1ee56a47ab17629ca1e7c25297e
Prathmesh Khedkar <prathmeshk1401@gmail.com>
2025-11-24 23:55:28 +0530

    Fi middleware issue

--- server.js
+++ server.js
@@ -50,8 +50,16 @@ app.use(
     })
 );
 
-// For preflight
-app.options("*", cors());
+app.use((req, res, next) => {
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 
 // -------------------------
 // Middleware
