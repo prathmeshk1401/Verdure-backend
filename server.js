f509a2656432d5daed5f661f40dfcc89d4456762
Prathmesh Khedkar <prathmeshk1401@gmail.com>
2025-11-25 01:25:46 +0530

    added one more ap

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ const allowedOrigins = [
     "https://verdure-frontend.vercel.app",
     "https://verdure-innovating-agriculture-hwmi-a15uvagtd.vercel.app",
     "https://verdure-innovating-agri-git-19e502-prathmeshs-projects-97d276a8.vercel.app",
-    "https://verdure-innovating-agriculture-hwmi-a9anwe8uv.vercel.app"
+    "https://verdure-innovating-agriculture-hwmi-a9anwe8uv.vercel.app",
+    "https://verdure-innovating-agriculture-hwmi-9seiuem6m.vercel.app"
 ];
 
 // -------------------------
